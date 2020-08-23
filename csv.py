b3e3efc1a1a054ec7d2bd666676f9f82b3da7a88
ibringpizza <52234395+ibringpizza@users.noreply.github.com>
2020-08-23 01:41:23 -0400

    Create csv.py
    sort csv files from a terminal

---
+++ csv.py
@@ -0,0 +1,30 @@
+import sys
+import operator
+import re
+
+if len(sys.argv) != 5 and sys.argv[1] != 'head':
+    print('python csv.py file.csv [column] [acending (a)/descending (d)] [itemstodisplay]')
+    print('python csv.py head file.csv')
+    sys.exit()
+
+if sys.argv[1] == 'head':
+    with open(sys.argv[2], 'r') as o:
+        for _ in range(10):
+            print(o.readline())
+    sys.exit()
+#regex - https://stackoverflow.com/a/2701015/8809174
+#https://stackoverflow.com/a/3513858/8809174
+
+lines = open(sys.argv[1], 'r').read().splitlines()
+ready = [re.compile(',\s*(?=(?:[^"]*"[^"]*")*[^"]*$)').split(line) for line in lines]
+for L in range(len(ready)):
+    for i in range(len(ready[L])):
+        try:
+            ready[L][i] = float(ready[L][i])
+        except Exception as e:
+            pass
+
+ready = sorted(ready, key=operator.itemgetter(int(sys.argv[2])), reverse=(sys.argv[3]=='d'))
+
+for line in ready[:int(sys.argv[4])]:
+    print(','.join([str(i) for i in line]))
